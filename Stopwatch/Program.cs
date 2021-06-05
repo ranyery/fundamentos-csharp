fde23240fb702a6e8fe482ee07deafec7cc62140
ranyery <ranyerysc@gmail.com>
2021-06-05 08:18:41 -0300

    Finaliza projeto Stopwatch

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Stopwatch
 {
@@ -6,19 +7,60 @@ namespace Stopwatch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu();
         }
 
-        static void Start() 
+        static void Menu()
         {
-            int time = 10;
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.Write("Quanto tempo deseja contar? ");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = type == 'm' ? 60 : 1;
+
+            if (time == 0) {
+                System.Environment.Exit(0);
+            }
+
+            PreStart(time * multiplier);
+
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+
+            Start(time);
+        }
+
+        static void Start(int time) 
+        {
             int currentTime = 0;
 
             while(currentTime != time)
             {
+                Console.Clear();
                 currentTime++;
                 Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
             }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado.");
+
+            Thread.Sleep(3000);
+            Menu();
         }
     }
 }
